535e49132231525bec2b9766d9737d0034482932
nehasharmaKrishna <nehasharma@krishnais.com>
2023-08-04 18:45:55 +0530

    Commit - visual composer

---
+++ wp-content/plugins/visualcomposer/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '45.4.x-dev',
+    'version' => '45.4.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a5e66660937b0cb81935101e9dc9d911cf1569a4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '45.4.x-dev',
+      'version' => '45.4.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a5e66660937b0cb81935101e9dc9d911cf1569a4',
+    ),
+  ),
+);
